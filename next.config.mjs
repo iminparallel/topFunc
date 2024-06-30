fcf80fc15c522d7594e53a943bc8ba907a3d88a9
iminparallel <ch.harit1995@gmail.com>
2024-06-30 23:14:58 +0530

    cors?

--- next.config.mjs
+++ next.config.mjs
@@ -3,7 +3,7 @@ const nextConfig = {
     async headers() {
         return [
           {
-            source: "/:path*",
+            source: "/api/:path*",
             headers: [
               { key: "Access-Control-Allow-Credentials", value: "true" },
               { key: "Access-Control-Allow-Origin", value: "*" }, // Change this to the specific origin you want to allow
