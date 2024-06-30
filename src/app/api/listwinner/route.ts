7fe38ee7390a41e4f6a4398cff0ca5585f9f7d03
iminparallel <ch.harit1995@gmail.com>
2024-06-30 18:20:22 +0530

    fingercrossed

--- src/app/api/listwinner/route.ts
+++ src/app/api/listwinner/route.ts
@@ -54,9 +54,11 @@ if(code===process.env.NEXT_PUBLIC_SECRET_CODE){
   const dateZeroTimeStamp = parseInt(Date.parse(dateZero).toString().slice(0,10));
   const dateMinusOneTimeStamp =  dateZeroTimeStamp - 24*60*60
   let assetList = {}
-  const assetDictionary = response.data.data.ticker
+ 
 
   const response = await axios.get(`https://api.kucoin.com/api/v1/market/allTickers/`);
+  const assetDictionary = response.data.data.ticker
+  /*console.log(assetDictionary)*/
   for (let i = 0; i < assetDictionary.length; i++) {
     assetList[assetDictionary[i].symbol] = assetDictionary[i].volValue
   }
