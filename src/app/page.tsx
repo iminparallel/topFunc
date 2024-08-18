71cead8297adc4bb4dab4dd80b1f33f95bf83440
iminparallel <ch.harit1995@gmail.com>
2024-08-18 12:29:00 +0530

    scrollable list

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,12 @@
-import Image from 'next/image'
-import {Winner} from '../components/winner'
-import {Submissions} from '../components/submissions'
+import Image from "next/image";
+import { Winner } from "../components/winner";
+import { Submissions } from "../components/submissions";
 import { CSSProperties } from "react";
 
-import type { Metadata } from 'next'
-import getAllAsset from '../lib/getAllAsset'
-import Search from '@/app/ui/search';
-import { useSearchParams, usePathname, useRouter } from 'next/navigation';
-
+import type { Metadata } from "next";
+import getAllAsset from "../lib/getAllAsset";
+import Search from "@/app/ui/search";
+import { useSearchParams, usePathname, useRouter } from "next/navigation";
 
 export default async function Home({
   searchParams,
@@ -17,73 +16,62 @@ export default async function Home({
     page?: string;
   };
 }) {
-
   const stylex: CSSProperties = {
-    position:"absolute",
-    bottom:400,
-    left:250,
-    width:"100%",
-  }
+    position: "absolute",
+    bottom: 400,
+    left: 250,
+    width: "100%",
+  };
 
   const style3: CSSProperties = {
-    position:"fixed",
-    top:0,
-    left:220,
-    width:"100%",
+    position: "absolute",
+    top: 0,
+    left: 220,
+    width: "100%",
   };
 
   const style_red: CSSProperties = {
-    left:200,
-}  
+    left: 200,
+  };
 
+  interface Listing {
+    symbol: string;
+  }
 
-interface Listing {
-  symbol: string;
-
-}
-
-  const query = searchParams?.query || '';
-  const assetData = getAllAsset()
-  const assets = await assetData
-  const coins = assets['data']['ticker']
-  let listings = []
-
-    
+  const query = searchParams?.query || "";
+  const assetData = getAllAsset();
+  const assets = await assetData;
+  const coins = assets["data"]["ticker"];
+  let listings = [];
 
   for (let i = 0; i < coins.length; i++) {
-      if (coins[i].symbol.toLowerCase().includes(query.toLowerCase()) )
-      {
-        listings.push(coins[i])
-      }
+    if (coins[i].symbol.toLowerCase().includes(query.toLowerCase())) {
+      listings.push(coins[i]);
     }
-
-
+  }
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between">
-     
-    <Winner/>
+      <Winner />
 
-    <section style = {style_red}  className="font-mono font-bold flex min-h-screen flex-col items-center justify-between">
-        <div >
-            <br/>
-        <ul style={style3}>
-        {listings.map((listing:Listing)=> {
-         return (
-            <li key={listing.symbol}>
-            <p >
-                    {listing.symbol} 
-            </p>
-            </li>
-         )
-        })}
-
-        </ul>  
-        </div>  
-        < Search/> 
-        </section>
-   
+      <section
+        style={style_red}
+        className="font-mono font-bold flex min-h-screen flex-col items-center justify-between"
+      >
+        <div>
+          <br />
+          <ul style={style3}>
+            {listings.map((listing: Listing) => {
+              return (
+                <li key={listing.symbol}>
+                  <p>{listing.symbol}</p>
+                </li>
+              );
+            })}
+          </ul>
+        </div>
+        <Search />
+      </section>
     </main>
-    
   );
 }
